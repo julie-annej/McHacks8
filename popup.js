7e805cb95d5380c51c95791d94d3ecd0b60c469b
julie-annej <60227045+julie-annej@users.noreply.github.com>
2021-01-30 14:22:16 -0500

    timer

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
-//
+let timeSpent = 0;
+let seconds = document.getElementById("seconds");
+
+function roundUp(num, precision) {
+  precision = Math.pow(10, precision);
+  return Math.ceil(num * precision) / precision;
+}
+
+function timecounter() {
+  function timer() {
+    var actualTime = performance.now();
+    seconds.value = roundUp(actualTime / 1000, 2);
+    requestAnimationFrame(timer);
+  }
+  requestAnimationFrame(timer);
+}
+
+const focus = function () {
+  timecounter();
+};
+const stop = function () {
+  timeSpent = seconds.value;
+  document.getElementById("data").innerHTML = timeSpent;
+};
+
+window.addEventListener("focus", focus);
+window.addEventListener("keydown", stop);
