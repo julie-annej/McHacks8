253c2b92d8d480ae09443a9cbca7d4e88af0da22
julie-annej <julie-annej@users.noreply.github.com>
2021-01-30 19:26:43 -0500

    events listenere

--- popup.js
+++ popup.js
@@ -1,44 +1,59 @@
 //Time
-
 var seconds = document.getElementById("seconds");
 var data = document.getElementById("data");
 var timer_on = 0;
-
+var temp=0;
+var totaltime;
+var previous=0;
 
 function roundUp(num, precision) {
     precision = Math.pow(10, precision)
     return Math.ceil(num * precision) / precision
-  };
+};
 
-function timecounter() {
-    if(timer_on == 1) {
+const start = function(){
+    if(totaltime == undefined){ 
+        totaltime = 0;
+    }
+
+    timer_on = 1;
+    if(timer_on) {
         requestAnimationFrame(timer);
     }
     function timer(){
         var actualTime = performance.now();
-        seconds.value = roundUp(actualTime/1000, 2);
+        seconds.value = roundUp(actualTime/1000, 2);  
         localStorage.setItem("time", seconds.value);
         requestAnimationFrame(timer);
     }
-    
-};
-
-const start = function(){
-    timer_on =1;
-    timecounter();
-    
 };
 
 const stop = function(){
+    
     timer_on = 0;
-    data.value = localStorage.getItem("time");
-
+    temp = localStorage.getItem("time");
+    
+    totaltime += Number(temp) - previous;
+    chrome.storage.sync.set({"time": temp, "totaltime": totaltime}, function(){
+         console.log("Time saved!"); 
+         console.log(temp);
+         console.log(totaltime);
+    });
+    previous = temp;
 };
 
+const reset = function(){
+    value = 0;
+    chrome.storage.sync.set({"totaltime": value});
+};
 
 window.addEventListener("focus", start);
-window.addEventListener("click", stop);
+chrome.runtime.onsuspend.addListener( function(){
+    
+    stop;
+    data.value = temp;
 
+});
 
 
 
